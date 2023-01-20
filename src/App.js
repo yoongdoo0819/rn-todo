0db9852692ef1869f4dea345640bc302076c7efa
krust <krust@krustui-MacBookPro-29.local>
2023-01-20 14:02:36 +0900

    NavigationContainer instead of View

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import SignInScreen from './screens/SignInScreen';
-import TestAvoid from './screens/TestAvoid';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar style="dark" />
       <SignInScreen></SignInScreen>
       {/* <TestAvoid></TestAvoid> */}
-    </View>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
 
 export default App;
