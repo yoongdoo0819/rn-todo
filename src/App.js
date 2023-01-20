490a1f39ec0128f93fabb7275ec13997a677cc5f
krust <krust@krustui-MacBookPro-29.local>
2023-01-20 14:25:47 +0900

    Navigator - native-stack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import SignInScreen from './screens/SignInScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './navigations/AuthStack';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar style="dark" />
-      <SignInScreen></SignInScreen>
+      <AuthStack></AuthStack>
       {/* <TestAvoid></TestAvoid> */}
     </NavigationContainer>
   );
---
+++ src/navigations/AuthStack.js
@@ -0,0 +1,15 @@
+//import { createNativeStackNavigator } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignInScreen from '../screens/SignInScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={'SignIn'} component={SignInScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
