24a8fbf9f32ee5ff97f205dddf7635c7f0111fc8
krust <krust@krustui-MacBookPro-29.local>
2023-01-21 20:21:07 +0900

    FlatList component

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,18 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { GRAY } from '../color';
+import ListItem from '../components/Listitem';
+
+const Separator = () => {
+  return <View style={styles.separator}></View>;
+};
 
 const ListScreen = () => {
+  const todos = [
+    { id: 1, task: 'React Native 1', isDone: false },
+    { id: 2, task: 'React Native 2', isDone: true },
+    { id: 3, task: 'React Native 3', isDone: false },
+    { id: 4, task: 'React Native 4', isDone: true },
+    { id: 5, task: 'React Native 5', isDone: false },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>ListScreen</Text>
-    </View>
+    <FlatList
+      data={todos}
+      renderItem={({ item }) => <ListItem item={item} />}
+      windowSize={5} // prev : 2 , curr : 1, next: 2
+      ItemSeparatorComponent={Separator}
+      //ListHeaderComponent={() => <View style={{ height: 10 }}></View>}
+      ListHeaderComponent={View}
+      ListHeaderComponentStyle={{ height: 10 }}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  separator: {
+    height: 1,
+    backgroundColor: GRAY.LIGHT,
+    marginVertical: 10,
+    marginHorizontal: 10,
   },
 });
 
